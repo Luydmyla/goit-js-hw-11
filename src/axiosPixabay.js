4eae6e73d3504ae4b905cde9b42885a9e585e985
Luydmyla <lytic.ok@gmail.com>
2022-01-13 00:08:52 +0200

    update files (work in progress)

--- src/axiosPixabay.js
+++ src/axiosPixabay.js
@@ -5,13 +5,15 @@
 // === именованный экспорт функции которая делает запрос на бекенд и возвращает промис
 import axios from 'axios';
 export function axiosPixabay(searchQuery) {
+  let pageNumber = 1;
   const params = {
     q: `${searchQuery}`,
     key: '25154920-bc2b97b916e9c15e1ff6fb5dd',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 40,
+    per_page: 4,
+    page: `${pageNumber}`,
   };
 
   return axios.get('https://pixabay.com/api/', {
--- src/index.js
+++ src/index.js
@@ -15,26 +15,34 @@ const refs = {
   form: document.querySelector('#search-form'),
   submitBtn: document.querySelector('#search-form button'),
   galleryEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  // infoItemdEl: document.querySelector('.info-item'),
 };
 // ===немного оформления элементов
 refs.form.style = 'background-color: #4056b4; display: flex; justify-content: center; padding: 8px';
 refs.submitBtn.style = 'margin-left: 32px';
+refs.loadMoreBtn.style =
+  ' display: flex; margin-top: 32px; margin-left: auto;  margin-right: auto; background-color: yellow';
+// refs.infoItemdEl.style = 'display: flex; justify-content: center';
+// console.log(refs.infoItemdEl);
 
 // ====повесила слушатель события  на форму для ввода на событие сабмит
 refs.form.addEventListener('submit', onSubmit);
-// refs.galleryEl.addEventListener('click');
+refs.galleryEl.addEventListener('click', onImageClick);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
+
 // const imageApiService = new ImageApiService();
 let imagesSearch = '';
 let searchQuery = '';
+let pageNumber = 1;
 // simpleLightbox();
 function createImageEl(imagesSearch) {
-  simpleLightbox();
   const markup = imagesSearch
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card">
        <a href="${largeImageURL}">
            <img src="${webformatURL}" alt="${tags}" loading = "lazy" /> </a>
-           <div class="info">
+           <div class="info" style= "display: flex; justify-content: space-around">
               <p class="info-item">
                  <b>Likes:</b>${likes}
               </p>
@@ -51,14 +59,22 @@ function createImageEl(imagesSearch) {
            </div>`;
     })
     .join('');
+
+  console.log(markup);
   refs.galleryEl.innerHTML = markup;
+  console.log(refs.galleryEl);
   // refs.galleryEl.insertAdjacentHTML('beforeend', markup);
 }
-function simpleLightbox() {
-  let gallery = new SimpleLightbox('.gallery a');
-  gallery.refresh();
-}
 
+// function simpleLightbox() {}
+function onImageClick(e) {
+  console.log('работает онклик');
+  e.preventDefault();
+  let lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+  });
+  lightbox.refresh();
+}
 function onSubmit(e) {
   // ===== запрет браузеру на перезагрузку страницы
   e.preventDefault();
@@ -70,14 +86,42 @@ function onSubmit(e) {
     //  обрабатываем вернувшийся результат (промис) с бекенда
     .then(image => {
       console.log('работает зен', image);
-      console.log(image.data);
+      // console.log(image.data);
       const imagesSearch = image.data.hits;
+      pageNumber = Number(image.config.params.page);
+      console.log(pageNumber);
       console.log(imagesSearch);
       if (imagesSearch.length === 0) {
         Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       } else {
         createImageEl(imagesSearch);
       }
+      pageNumber += 1;
+    })
+    // === цепляю метод для обработки ошибки
+    .catch(error => console.log(error));
+}
+function onLoadMoreClick(e) {
+  // ===== запрет браузеру на перезагрузку страницы
+  e.preventDefault();
+  console.log('работает');
+  console.log(pageNumber);
+  // pageNumber = pageNumber + 1;
+  // console.log(pageNumber);
+
+  axiosPixabay(searchQuery)
+    //  обрабатываем вернувшийся результат (промис) с бекенда
+    .then(image => {
+      console.log('работает зен2', image);
+      pageNumber += 1;
+      // console.log(image.data);
+      // const imagesSearch = image.data.hits;
+      // console.log(imagesSearch);
+      // if (imagesSearch.length === 0) {
+      //   Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      // } else {
+      //   createImageEl(imagesSearch);
+      // }
     })
     // === цепляю метод для обработки ошибки
     .catch(error => console.log(error));
