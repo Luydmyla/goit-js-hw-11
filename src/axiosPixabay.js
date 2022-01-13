d984df304d7112fe3238474f7d63a33395eb6808
Luydmyla <lytic.ok@gmail.com>
2022-01-13 02:31:49 +0200

    update hw-11 ()work in progress)

--- src/axiosPixabay.js
+++ src/axiosPixabay.js
@@ -1,31 +1,52 @@
-// export default class ImageApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//   }
-// === именованный экспорт функции которая делает запрос на бекенд и возвращает промис
-import axios from 'axios';
-export function axiosPixabay(searchQuery) {
-  let pageNumber = 1;
-  const params = {
-    q: `${searchQuery}`,
-    key: '25154920-bc2b97b916e9c15e1ff6fb5dd',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 4,
-    page: `${pageNumber}`,
-  };
+const API_KEY = '25154920-bc2b97b916e9c15e1ff6fb5dd';
+const BASE_URL = 'https://pixabay.com/api';
 
-  return axios.get('https://pixabay.com/api/', {
-    params,
-  });
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchFotos() {
+    console.log(this);
+
+    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.icrementPage();
+        console.log(data);
+        return data.hits;
+      });
+  }
+  icrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.query - newQuery;
+  }
 }
+// пробовала через аксиос, пока еще не получилось
+// // === именованный экспорт функции которая делает запрос на бекенд и возвращает промис
+// import axios from 'axios';
+// export function axiosPixabay(searchQuery) {
+//   let pageNumber = 1;
+//   const params = {
+//     q: `${searchQuery}`,
+//     key: '25154920-bc2b97b916e9c15e1ff6fb5dd',
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: 4,
+//     page: `${pageNumber}`,
+//   };
 
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.query - newQuery;
-//   }
+//   return axios.get('https://pixabay.com/api/', {
+//     params,
+//   });
 // }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { axiosPixabay } from './axiosPixabay';
 import ImageApiService from './axiosPixabay';
-
 // установила симпллайтбокс, нотифай и аксиос зарегестрировалась на  пиксабей
 // нашла ссылки на элементы формы и кнопки
 const refs = {
@@ -31,18 +30,15 @@ refs.form.addEventListener('submit', onSubmit);
 refs.galleryEl.addEventListener('click', onImageClick);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
 
-// const imageApiService = new ImageApiService();
-let imagesSearch = '';
-let searchQuery = '';
-let pageNumber = 1;
-// simpleLightbox();
-function createImageEl(imagesSearch) {
-  const markup = imagesSearch
+const imageApiService = new ImageApiService();
+// console.log(imageApiService);
+function createImageEl(hits) {
+  const markup = hits
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `<div class="photo-card">
+      return `<div class="photo-card >
        <a href="${largeImageURL}">
-           <img src="${webformatURL}" alt="${tags}" loading = "lazy" /> </a>
-           <div class="info" style= "display: flex; justify-content: space-around">
+           <img src="${webformatURL}" alt="${tags}" loading = "lazy" width = "480"/> </a>
+           <div class="info" style= "display: flex; justify-content: center">
               <p class="info-item">
                  <b>Likes:</b>${likes}
               </p>
@@ -66,63 +62,65 @@ function createImageEl(imagesSearch) {
   // refs.galleryEl.insertAdjacentHTML('beforeend', markup);
 }
 
-// function simpleLightbox() {}
+function simpleLightbox() {}
 function onImageClick(e) {
   console.log('работает онклик');
   e.preventDefault();
-  let lightbox = new SimpleLightbox('.gallery a', {
-    /* options */
-  });
-  lightbox.refresh();
 }
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+lightbox.refresh();
+
 function onSubmit(e) {
   // ===== запрет браузеру на перезагрузку страницы
   e.preventDefault();
-  searchQuery = e.currentTarget.elements.searchQuery.value;
-  console.log(searchQuery);
-  // imagesSearch = e.currentTarget;
-  // console.log(imagesSearch);
-  axiosPixabay(searchQuery)
+
+  imageApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+  console.log(imageApiService.searchQuery);
+  imageApiService.resetPage();
+  if (imageApiService.searchQuery === '') {
+    Notify.warning('Ведите запрос');
+  } else {
     //  обрабатываем вернувшийся результат (промис) с бекенда
-    .then(image => {
-      console.log('работает зен', image);
-      // console.log(image.data);
-      const imagesSearch = image.data.hits;
-      pageNumber = Number(image.config.params.page);
-      console.log(pageNumber);
-      console.log(imagesSearch);
-      if (imagesSearch.length === 0) {
+    imageApiService.fetchFotos().then(hits => {
+      console.log(hits);
+      console.log('работает зен');
+      clearImagesContainer();
+      if (hits.length === 0) {
         Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       } else {
-        createImageEl(imagesSearch);
+        createImageEl(hits);
       }
-      pageNumber += 1;
-    })
-    // === цепляю метод для обработки ошибки
-    .catch(error => console.log(error));
+    });
+  }
 }
+// === цепляю метод для обработки ошибки
+// .catch(error => console.log(error));
+
 function onLoadMoreClick(e) {
   // ===== запрет браузеру на перезагрузку страницы
   e.preventDefault();
   console.log('работает');
-  console.log(pageNumber);
-  // pageNumber = pageNumber + 1;
-  // console.log(pageNumber);
-
-  axiosPixabay(searchQuery)
-    //  обрабатываем вернувшийся результат (промис) с бекенда
-    .then(image => {
-      console.log('работает зен2', image);
-      pageNumber += 1;
-      // console.log(image.data);
-      // const imagesSearch = image.data.hits;
-      // console.log(imagesSearch);
-      // if (imagesSearch.length === 0) {
-      //   Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      // } else {
-      //   createImageEl(imagesSearch);
-      // }
-    })
-    // === цепляю метод для обработки ошибки
-    .catch(error => console.log(error));
+  imageApiService.fetchFotos().then(hits => createImageEl(hits));
 }
+function clearImagesContainer() {
+  refs.galleryEl.innerHTML = '';
+}
+
+// axiosPixabay(searchQuery)
+//   //  обрабатываем вернувшийся результат (промис) с бекенда
+//   .then(image => {
+//     console.log('работает зен2', image);
+//     pageNumber += 1;
+//     // console.log(image.data);
+//     // const imagesSearch = image.data.hits;
+//     // console.log(imagesSearch);
+//     // if (imagesSearch.length === 0) {
+//     //   Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//     // } else {
+//     //   createImageEl(imagesSearch);
+//     // }
+//   })
+//   // === цепляю метод для обработки ошибки
+//   .catch(error => console.log(error));
